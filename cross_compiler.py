120af6122e9e1bcc937c49f8c3ef4a4843ad4d5b
Andrew Danilovic <a.danilovic@ucla.edu>
2018-04-14 21:45:06 -0700

    Fix gitignore, add unzip

--- cross_compiler.py
+++ cross_compiler.py
@@ -24,7 +24,7 @@ class Dir:
         """Create all directories if they don't already exist"""
         for comp in components:
             pathlib.Path(os.path.join(self.abspath, comp)).mkdir(parents=True, exist_ok=True)
-        
+
 class BuildDir(Dir):
     def __init__(self, basedir, components):
         super(BuildDir, self).__init__(os.path.join(basedir, 'build'), components)
@@ -56,30 +56,30 @@ class FTPSite(FTP):
         self.sitename = sitename
         self.dirname  = dirname
         self.filename = filename
+
+        self.ftpfilename = self.dirname + self.filename
+
         self.url = URL(sitename + dirname + filename)
         print('FTPSite')
         print('\tsitename:\t', self.sitename)
         print('\tdirname:\t',  self.dirname)
         print('\tfilename:\t', self.filename)
+
+
+        print('\tftpfilename:\t', self.ftpfilename)
+
         self.url.print_url()
 
-    def get_localpath(self):
+    def get_local_dir_from_ftp_dir(self):
         return os.path.join(self.dirname.lstrip(os.path.sep))
 
-class GNU(FTPSite):
-
-    def __init__(self, subdir, filename):
-        super(GNU, self).__init__(sitename='ftp.gnu.org',
-                                  dirname='/gnu' + subdir,
-                                  filename=filename)
-
-    def dir_callback(self, dir_listing):
-        self.versions.append(dir_listing)        
-
     def download(self, basedir):
-        self.localdirname = os.path.join(basedir, self.get_localpath())
+
+        self.localdirname = os.path.join(basedir, self.get_local_dir_from_ftp_dir())
         self.localfilename = self.localdirname + self.filename
-        self.ftpfilename = self.dirname + self.filename
+
+        print('\tlocaldirname:\t', self.localdirname)
+        print('\tlocalfilename:\t', self.localfilename)
 
         if not os.path.exists(self.localfilename):
             with open(self.localfilename, 'wb') as f:
@@ -91,11 +91,22 @@ class GNU(FTPSite):
                 FTP.retrbinary(self, cmd='RETR {}'.format(self.ftpfilename), callback=callback)
                 self.quit
 
-#            subprocess.check_call(['tar',
-#                                   '-xvf',
-#                                   self.filename,
-#                                   '-C',
-#                                   os.path.dirname(self.filename)])
+    def unzip(self):
+        subprocess.check_call(['tar',
+                               '-xvf',
+                               self.localfilename,
+                               '-C',
+                               self.localdirname])
+
+class GNU(FTPSite):
+
+    def __init__(self, subdir, filename):
+        super(GNU, self).__init__(sitename='ftp.gnu.org',
+                                  dirname='/gnu' + subdir,
+                                  filename=filename)
+
+    def dir_callback(self, dir_listing):
+        self.versions.append(dir_listing)
 
     def build(self):
         self.builddir   = os.path.join(self.localdirname, 'build')
@@ -125,7 +136,7 @@ class GCC(GNU):
     #configure: error: Building GCC requires GMP 4.2+, MPFR 2.4.0+ and MPC 0.8.0+.
     #./contrib/download_prerequisites
 
-    def __init__(self):        
+    def __init__(self):
         super(GCC, self).__init__(subdir='/gcc/gcc-7.3.0',
                                   filename='/gcc-7.3.0.tar.gz')
         configureargs = '--enable-languages=c,c++'
@@ -153,11 +164,15 @@ class CrossCompiler():
         self.basedir = basedir
 
         self.components = [GCC(), BinUtils()]
-        self.bdir = BuildDir(self.basedir, [path.get_localpath() for path in self.components])
+        self.bdir = BuildDir(self.basedir, [path.get_local_dir_from_ftp_dir() for path in self.components])
 
     def download(self):
         for component in self.components:
             component.download(self.bdir.abspath)
+
+    def unzip(self):
+        for component in self.components:
+            component.unzip()
 
     def build(self):
         #self.binutils.build()
@@ -176,6 +191,7 @@ def main():
                           os.path.abspath(os.getcwd()))
 
     arm_cc.download()
+    arm_cc.unzip()
 
 if __name__ == '__main__':
     main()
